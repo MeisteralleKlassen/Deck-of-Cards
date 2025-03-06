6f06265458a84faf56578dfd681833ae34098aa3
MeisteralleKlassen <117626699+MeisteralleKlassen@users.noreply.github.com>
2025-03-06 12:47:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function updateLeaderboard() {
 // Service Worker registrieren
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        navigator.serviceWorker.register('./service-worker.js').then((registration) => {
             console.log('Service Worker registriert:', registration);
         }).catch((error) => {
             console.log('Service Worker Registrierung fehlgeschlagen:', error);
